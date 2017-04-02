f67577a5fe0eeeb57863963277e330a4f9421528
Will Essilfie <wke2102@columbia.edu>
2017-04-01 23:42:12 -0400

    adding string concatenation

--- src/strops.c
+++ src/strops.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
+
+// taken from http://stackoverflow.com/questions/14259540/c-programming-strcat-using-pointer
 char * addstr(char *x, char *y)
 {
-	return "";
+	int string_length = strlen(x) + strlen(y);
+	char *temporary = (char *)malloc(sizeof(char)* string_length);
+	append(temporary, s)
+	append(temporary, t)
+
+	return temporary;
 }
+
+void append(char *y , char *x) {   
+     //move pointer t to end of the string it points. 
+    while(*y != '\0'){
+        y++;
+    }
+
+    while( *x != '\0' ){
+        *y = *x;
+        y++;
+        x++;    
+    }       
+}  
 
 #ifdef BUILD_TEST
 int main()
